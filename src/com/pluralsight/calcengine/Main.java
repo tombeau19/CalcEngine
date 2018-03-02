1f7de1aff826ff7d840592dacd481f540fda1a77
tombeau19 <thomas.beauregard@gmail.com>
2018-03-02 12:02:55 -0500

    added MathEquation class and methods

--- src/com/pluralsight/calcengine/Main.java
+++ src/com/pluralsight/calcengine/Main.java
@@ -4,37 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 
-	    double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-	    double[] rightVals  = {50.0d, 92.0d, 17.0d, 3.0d};
-	    char[] opCodes = {'d', 'a', 's', 'm'};
-		double[] results = new double[opCodes.length];
+//	    double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+//	    double[] rightVals  = {50.0d, 92.0d, 17.0d, 3.0d};
+//	    char[] opCodes = {'d', 'a', 's', 'm'};
+//		double[] results = new double[opCodes.length];
 
-		for(int i = 0; i <opCodes.length; i++) {
-			switch(opCodes[i]) {
-				case 'a':
-					results[i] = leftVals[i] + rightVals[i];
-					break;
-				case 's':
-					results[i] = leftVals[i] - rightVals[i];
-					break;
-				case 'd':
-					results[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-					break;
-				case 'm':
-					results[i] = rightVals[i] * leftVals[i];
-					break;
-				default:
-					System.out.println("Error - invalid opCode");
-					results[i] = 0.0d;
-					break;
-			}
-		}
+		MathEquation[] equations = new MathEquation[4];
+		equations[0] = create(100.0d, 50.0d, 'd');
+		equations[1] = create(25.0d, 92.0d, 'a');
+		equations[2] = create(225.0d, 17.0d, 's');
+		equations[3] = create(11.0d, 3.0d, 'm');
 
-
-		for(double theResult : results) {
+		for(MathEquation equation : equations) {
+			equation.execute();
 			System.out.print("result = ");
-			System.out.println(theResult);
+			System.out.println(equation.result);
 		}
 
     }
+
+    public static MathEquation create(double leftVal, double rightVal, char opCode) {
+    	MathEquation equation = new MathEquation();
+    	equation.leftVal = leftVal;
+    	equation.rightVal = rightVal;
+    	equation.opCode = opCode;
+    	return equation;
+	}
 }
---
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -0,0 +1,31 @@
+package com.pluralsight.calcengine;
+
+public class MathEquation {
+
+    public double leftVal;
+    public double rightVal;
+    public char opCode;
+    public double result;
+
+    public void execute() {
+        switch(opCode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+                break;
+            case 'm':
+                result = rightVal * leftVal;
+                break;
+            default:
+                System.out.println("Error - invalid opCode");
+                result = 0.0d;
+                break;
+        }
+    }
+
+}
